52fc8346d068026b4610817a05a9983b69608650
Muhammad Haseeb Ur rehman <rehmanhaseeb2345@gmail.com>
2026-08-04 06:58:21 +0100

    refactor: comment out unused string manipulation code for clarity

--- 02-arrays-and-strings/01-Strings.js
+++ 02-arrays-and-strings/01-Strings.js
@@ -1,20 +1,32 @@
-let s = "helol";
-let slow = 0;
-let fast = 1;
+// let s = "helol";
+// let slow = 0;
+// let fast = 1;
 
-while (slow < s.length) {
-  if (fast == s.length) {
-    slow++;
-    fast = slow + 1;
-    continue;
+// while (slow < s.length) {
+//   if (fast == s.length) {
+//     slow++;
+//     fast = slow + 1;
+//     continue;
+//   }
+
+//   if (s[slow] == s[fast]) {
+//     console.log(s[slow]);
+//     slow++;
+//     fast = slow + 1;
+//     continue;
+//   }
+
+//   fast++;
+// }
+
+function fibonacci(n) {
+  // Base case: if n is 0 or 1, just return n
+  if (n <= 1) {
+    return n;
   }
 
-  if (s[slow] == s[fast]) {
-    console.log(s[slow]);
-    slow++;
-    fast = slow + 1;
-    continue;
-  }
-
-  fast++;
+  // The function calls ITSELF twice!
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+console.log(fibonacci(2100)); // Output: 10946
