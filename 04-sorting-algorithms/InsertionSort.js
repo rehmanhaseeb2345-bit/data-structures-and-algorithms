8b1f360f9a17166dc394341f62f6b2329c05b037
Muhammad Haseeb Ur rehman <rehmanhaseeb2345@gmail.com>
2026-08-07 22:40:31 +0100

    Opposite way

--- 04-sorting-algorithms/InsertionSort.js
+++ 04-sorting-algorithms/InsertionSort.js
@@ -1,4 +1,4 @@
-let arr = [5, 9, 3, 6];
+// let arr = [5, 9, 3, 6];
 // for (let i = 1; i < arr.length; i++) {
 //   let current = arr[i];
 //   let j = i - 1;
@@ -11,11 +11,29 @@ let arr = [5, 9, 3, 6];
 // }
 // console.log(arr);
 
+// function InsertionSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     let current = arr[i];
+//     let j = i - 1;
+//     while (j >= 0 && arr[j] > current) {
+//       arr[j + 1] = arr[j];
+//       j--;
+//     }
+//     arr[j + 1] = current;
+//   }
+//   return arr;
+// }
+
+// console.log(InsertionSort(arr));
+
+let arr = [1, 2, 3, 4, 5, 0];
+
 function InsertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
+  for (i = 1; i < arr.length; i++) {
     let current = arr[i];
     let j = i - 1;
-    while (j >= 0 && arr[j] > current) {
+
+    while (j >= 0 && arr[j] < current) {
       arr[j + 1] = arr[j];
       j--;
     }
@@ -23,5 +41,4 @@ function InsertionSort(arr) {
   }
   return arr;
 }
-
 console.log(InsertionSort(arr));
