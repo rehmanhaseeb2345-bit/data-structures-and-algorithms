42a9b9a7518154358dace785c13d4eebaf6727fc
Muhammad Haseeb Ur rehman <rehmanhaseeb2345@gmail.com>
2026-08-07 20:54:51 +0100

    feat: implement InsertionSort algorithm with example usage

---
+++ 04-sorting-algorithms/InsertionSort.js
@@ -0,0 +1,27 @@
+let arr = [5, 9, 3, 6];
+// for (let i = 1; i < arr.length; i++) {
+//   let current = arr[i];
+//   let j = i - 1;
+//   while (j >= 0 && arr[j] > current) {
+//     arr[j + 1] = arr[j];
+//     j--;
+//   }
+
+//   arr[j + 1] = current;
+// }
+// console.log(arr);
+
+function InsertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
+
+console.log(InsertionSort(arr));
