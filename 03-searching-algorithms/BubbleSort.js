dcbe4b3e8b631a1130b4ae024cfe7483bd031d9e
Muhammad Haseeb Ur rehman <rehmanhaseeb2345@gmail.com>
2026-08-07 13:38:02 +0100

    refactor: update BubbleSort function to sort in descending order and clean up commented code

--- 03-searching-algorithms/BubbleSort.js
+++ 03-searching-algorithms/BubbleSort.js
@@ -1,16 +1,39 @@
-function bubbleSort(arr) {
+// function bubbleSort(arr) {
+//   let n = arr.length;
+//   let swapped;
+
+//   for (i = 0; i < n; i++) {
+//     swapped = false;
+//     for (j = 0; j < n - i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//         swapped = true;
+//       }
+//     }
+
+//     if (!swapped) {
+//       break;
+//     }
+//   }
+//   return arr;
+// }
+
+// let arr = [7, 3, 6, 1];
+// console.log("Original:", arr);
+// console.log("Sorted:", bubbleSort(arr));
+
+function BubbleSort(arr) {
   let n = arr.length;
   let swapped;
 
-  for (i = 0; i < n; i++) {
+  for (i = n - 1; i >= 0; i--) {
     swapped = false;
-    for (j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] < arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         swapped = true;
       }
     }
-
     if (!swapped) {
       break;
     }
@@ -18,6 +41,5 @@ function bubbleSort(arr) {
   return arr;
 }
 
-let arr = [7, 3, 6, 1];
-console.log("Original:", arr);
-console.log("Sorted:", bubbleSort(arr));
+let arr = [50, 22, 45, 11, 34, 9];
+console.log(BubbleSort(arr));
